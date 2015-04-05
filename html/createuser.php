946a04e50287dc17349cce664183feaf8850fa7e
awesomeyi <awesomeyi@gmail.com>
2015-04-04 21:57:31 -0400

    Improved input validation

--- html/createuser.php
+++ html/createuser.php
@@ -1,5 +1,9 @@
 <?php
 	$usertype = $_POST['usertype'];
+	if(!($usertype == 'patient' || $usertype == 'caretaker')) {
+		echo 'Error';
+		exit();
+	}
 	$db = new mysqli('localhost', 'super', '0x4D4853', 'memorymom');
 	$stmt = $db->prepare('INSERT INTO user VALUES (NULL, ?)');
 	$stmt->bind_param('s', $usertype);

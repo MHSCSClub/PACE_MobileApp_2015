a77299e0cc7bdf82e9812d97d7296dbd620c5af1
jackphillips7764 <jackphillips@Jacks-iMac.local>
2015-04-10 22:35:59 -0400

    Add Events
    Got Adding Events done

--- Memory Moments/Memory Moments/addEventViewController.swift
+++ Memory Moments/Memory Moments/addEventViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Foundation
+class addEventViewController: UIViewController, UIPickerViewDataSource, UITextViewDelegate, UITextFieldDelegate {
 
-class addEventViewController: UIViewController {
-
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var eventTitle: UITextField!
+    @IBOutlet var textField: UITextView!
+    @IBOutlet var Type: UIPickerView!
+    var Edit = false;
+    var typeOptions = ["Event", "Reminder"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        textField.delegate = self;
+        textField.layer.borderWidth = 1;
+        textField.layer.borderColor = UIColor.grayColor().CGColor
+        eventTitle.delegate = self;
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +30,69 @@ class addEventViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+
+    @IBAction func endEdit(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    @IBAction func RemoveKeyBoard(sender: AnyObject) {
+        
+        eventTitle.resignFirstResponder()
+    }
+    //required for picker view 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typeOptions.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        println(typeOptions[row])
+        return typeOptions[row]
+    }
+    //connects to database and adds Event
+    @IBAction func DoneAdd(sender: AnyObject) {
+        var pid: String = "";
+        if let dirs : [String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
+            let dir = dirs[0] //documents directory
+            let path = dir.stringByAppendingPathComponent("PID.txt");
+            
+            //reading to get the PID of person
+            pid = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)!
+            println(pid)
+        }
+        datePicker.timeZone = NSTimeZone(name: "Etc/UTC")
+        let time = datePicker.date
+        print("Date \(time)")
+        //sets up and makes conection to the database
+        var url: NSURL = NSURL(string: "http://aakatz3.asuscomm.com:8085/mobile/createevent.php")!
+        var request:NSMutableURLRequest = NSMutableURLRequest(URL:url)
+        var bodyData = "pid=\(pid)&time=\(time)&type=\(typeOptions[Type.selectedRowInComponent(0)])&title=\(eventTitle.text)&description=\(textField.text)"
+        request.HTTPMethod = "POST"
+        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())
+            {
+                (response, data, error) in
+                println("\(data)")
+                //makes sure that the data base actually sent something back
+                if(data == nil){
+                    print("Server connection failed");
+                    return;
+                }
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewControllerWithIdentifier("CareGiversViewController") as! UIViewController
+                self.presentViewController(vc, animated: false, completion: nil)
+                return;
+            }
+        let alertController = UIAlertController(title: "Problem", message:
+            "Server Connection Problem\nPlease try again", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
+    }
 
     /*
     // MARK: - Navigation

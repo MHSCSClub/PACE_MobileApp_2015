46c2ea821c0e690894e945b36d0ef75f1ada469c
jackphillips7764 <jjadav@macairs-MacBook-Air.local>
2015-04-13 16:10:37 -0400

    add screen

---
+++ Memory Moments/Memory Moments/AddFlashCardsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddFlashCardsViewController.swift
+//  Memory Moments
+//
+//  Created by Jadav, Jigar on 4/13/15.
+//  Copyright (c) 2015 Jack Phillips. All rights reserved.
+//
+
+import UIKit
+
+class AddFlashCardsViewController: UIViewController {
+    @IBOutlet weak var InformationBox: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        InformationBox.layer.borderWidth = 1;
+        InformationBox.layer.borderColor = UIColor.grayColor().CGColor
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
